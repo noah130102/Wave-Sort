31f565acfc33e04c0476b9f28d17e676057381ee
Karandeep Singh <110446557+noah130102@users.noreply.github.com>
2022-10-04 18:48:01 +0530

    Add files via upload

---
+++ waveSort.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+void waveSort(int arr[], int n){
+
+    for (int i = 1; i < n-1; i+=2)
+    {
+        if (arr[i]>arr[i-1])
+        {
+            swap(arr[i],arr[i-1]);
+        }
+        if (arr[i]>arr[i+1])
+        {
+            swap(arr[i],arr[i+1]);
+        }
+        
+    }
+    
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    int arr[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin>>arr[i];
+    }
+    waveSort(arr,n);
+
+    for (int  i = 0; i < n; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
